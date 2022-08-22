9577b715560b964716771c7ac4f85d750fb4e80d
Anton Bystrov <tony.against.machine@gmail.com>
2022-08-23 01:17:02 +0300

    r2d2: methods moved to actions module

--- r2d2/actions.py
+++ r2d2/actions.py
@@ -1,7 +1,9 @@
 import webbrowser
-import main
+
 
 def open_youtube():
     return webbrowser.open('https://www.youtube.com/')
 
 
+def search_query(search_obj):
+    return webbrowser.open_new_tab('https://www.google.com/search?q={}'.format(search_obj))
--- r2d2/main.py
+++ r2d2/main.py
@@ -1,12 +1,7 @@
 import speech_recognition as sr
-import webbrowser
+import actions
 
 speech_recognizer = sr.Recognizer()
-
-
-def search_query(search_obj):
-    return webbrowser.open_new_tab('https://www.google.com/search?q={}'.format(search_obj))
-
 
 with sr.Microphone() as source:
     audio = speech_recognizer.listen(source)
@@ -16,4 +11,6 @@ for word in query:
     if word == 'найди':
         query.remove('найди')
         search_obj = '+'.join(query)
-        search_query(search_obj=search_obj)
+        actions.search_query(search_obj=search_obj)
+    else:
+        print('настрой нормально звук и цикл')
