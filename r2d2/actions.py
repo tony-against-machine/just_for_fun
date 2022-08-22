0699caa91119411f080426818b29c12937cf8d49
Anton Bystrov <tony.against.machine@gmail.com>
2022-08-22 04:57:49 +0300

    search test

--- r2d2/actions.py
+++ r2d2/actions.py
@@ -3,3 +3,6 @@ import webbrowser
 
 def open_web_browser():
     return webbrowser.open('https://www.youtube.com/')
+
+def search_query():
+    return webbrowser.open_new_tab('https://www.google.com/search?q=прогноз+погоды')
--- r2d2/main.py
+++ r2d2/main.py
@@ -8,7 +8,11 @@ with sr.Microphone() as source:
     audio = speech_recognizer.listen(source)
     query = speech_recognizer.recognize_google(audio_data=audio, language='ru-RU').lower()
 
+
+
 if query == 'открой youtube':
     print(actions.open_web_browser())
+if query == 'найди прогноз погоды':
+    print(actions.search_query())
 else:
     print('настрой нормально уровень шума и таймаут!')
